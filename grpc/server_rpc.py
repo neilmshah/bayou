8b003b593063c1a983c822d04ebcc6711c99b1b6
Shabari Girish Ganapathy <shabari8695@gmail.com>
2018-10-25 06:18:34 -0700

    functional two server anti-entropy with one alternate check

--- grpc/server_rpc.py
+++ grpc/server_rpc.py
@@ -30,11 +30,17 @@ def init_dummy(dummy_data):
     calendar_entry["room_no"] = str(one[0])
     calendar_entry["booking_date"] = str(one[1])
     calendar_entry["start_time"] = str(one[2])
-    calendar_entry["timestamp"] = str(one[3])
+    calendar_entry["alternate1_booking_date"] = str(one[3])
+    calendar_entry["alternate1_start_time"] = str(one[4])
+    calendar_entry["timestamp"] = str(one[5])
     calendar_entry["booking_status"] = str("tentative")
-    calendar_entry["id"] = str(one[4])
+    calendar_entry["id"] = str(one[6])
     #print(calendar_entry)
     booking_list.append(calendar_entry)
+
+    print("Initial Entries")
+    print(calendar_entry["room_no"]+" "+calendar_entry["room_no"]+" "+calendar_entry["start_time"])
+
 
     calendar_entry = {}
 
@@ -42,12 +48,15 @@ def init_dummy(dummy_data):
     calendar_entry["room_no"] = str(two[0])
     calendar_entry["booking_date"] = str(two[1])
     calendar_entry["start_time"] = str(two[2])
+    calendar_entry["alternate1_booking_date"] = ""
+    calendar_entry["alternate1_start_time"] = ""
     calendar_entry["timestamp"] = str(two[3])
     calendar_entry["booking_status"] = "tentative"
     calendar_entry["id"] = str(two[4])
 
     #print(calendar_entry)
     booking_list.append(calendar_entry)
+
 
 
 def yield_entries():
@@ -63,6 +72,8 @@ def yield_entry():
                                         room_no = item["room_no"],
                                         b_date = item["booking_date"],
                                         b_time = item["start_time"],
+                                        a1_date = item["alternate1_booking_date"],
+                                        a1_time = item["alternate1_start_time"],
                                         timestamp = item["timestamp"],
                                         status = item["booking_status"])
 
@@ -84,53 +95,94 @@ def does_entry_exists(id):
     
     return False
 
+def checkbooking(booking_info):
+    global booking_list
+    bookingAvail = True
+    for i in range(0,len(booking_list)):
+        eachBooking = booking_list[i]
+
+        if(eachBooking["id"] == booking_info["id"]):
+            print("id match")
+            bookingAvail = False
+            break
+
+
+        if(eachBooking["room_no"]==booking_info["room_no"] and eachBooking["booking_date"]==booking_info["booking_date"]):
+            if((eachBooking["start_time"] == booking_info["start_time"]) and (eachBooking["timestamp"] < booking_info["timestamp"]) ): 
+                print("start time of server list less")
+                bookingAvail=False
+                break
+            if ((eachBooking["start_time"] == booking_info["start_time"]) and (eachBooking["timestamp"] > booking_info["timestamp"]) ):
+                eachBooking['start_date'] = eachBooking["alternate1_booking_date"]
+                eachBooking['start_time'] = eachBooking["alternate1_start_time"]
+                eachBooking["alternate1_booking_date"] = ""
+                eachBooking["alternate1_start_time"] = ""
+                booking_list.append(eachBooking)
+
+                del booking_list[i]
+
+    return bookingAvail
+
 class BayouServer(a_e_pb2_grpc.BayouServicer):
     
     def __init__(self):
         self.new_list = []
+
+    def make_new_object(self,request):
+        calendar_entry = {}
+
+        calendar_entry["username"] = request.username
+        calendar_entry["room_no"] = request.room_no
+        calendar_entry["booking_date"] = request.b_date
+        calendar_entry["start_time"] = request.b_time
+        calendar_entry["alternate1_booking_date"] = request.a1_date
+        calendar_entry["alternate1_start_time"] = request.a1_time
+        calendar_entry["timestamp"] = request.timestamp
+        calendar_entry["booking_status"] = request.status
+        calendar_entry["id"] = request.messageid
+
+        return calendar_entry
+
 
     def anti_entropy(self,request_iterator,context):
         self.new_list = []
         global booking_list
 
         for request in request_iterator:
-            calendar_entry = {}
             #print(request.messageid)
 
-            calendar_entry["username"] = request.username
-            calendar_entry["room_no"] = request.room_no
-            calendar_entry["booking_date"] = request.b_date
-            calendar_entry["start_time"] = request.b_time
-            calendar_entry["timestamp"] = request.timestamp
-            calendar_entry["booking_status"] = request.status
-            calendar_entry["id"] = request.messageid
+            calendar_entry = self.make_new_object(request)
 
             #print(calendar_entry)
-            
+            """
             flag = True
             for item in booking_list:
 
                 if item['id'] == calendar_entry['id']:
-                    print("this is called-1")
+                    #print("this is called-1")
                     flag = False
                     break
 
                 if item['room_no'] == calendar_entry['room_no'] and item['start_time'] == calendar_entry['start_time']:
                     if item['timestamp'] <= calendar_entry['timestamp']:
-                        print('this is called-2')
+                        #print('this is called-2')
                         flag = False
                         break
                     else:
                         #update the new values to the existing one
                         pass
-
-            if flag:
-                print('this is called-3') 
+            """
+            if checkbooking(calendar_entry):
+                #print('this is called-3') 
                 self.new_list.append(calendar_entry)
 
         booking_list += self.new_list
 
-        print(len(booking_list))
+        #print(len(booking_list))
+        for item in booking_list:
+            print(item["id"]+" "+item["booking_date"]+" "+item["start_time"])
+
+        print("$$")
 
         for item in booking_list:
 
@@ -139,6 +191,8 @@ class BayouServer(a_e_pb2_grpc.BayouServicer):
                                         room_no = item["room_no"],
                                         b_date = item["booking_date"],
                                         b_time = item["start_time"],
+                                        a1_date = item["alternate1_booking_date"],
+                                        a1_time = item["alternate1_start_time"],
                                         timestamp = item["timestamp"],
                                         status = item["booking_status"])
 
